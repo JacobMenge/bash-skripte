0474057af6c259ca8af82c15475ee975b974895b
Jacob B. Menge <36003126+JacobMenge@users.noreply.github.com>
2023-04-24 10:55:45 +0200

    Update client.sh

--- server-client/client.sh
+++ server-client/client.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-echo running
-for i in {1..10000}; do
-  echo "Hello World"
-  read response
-done
+
+# IP-Adresse und portnummer des Servers
+SERVER_IP="127.0.0.1"
+SERVER_PORT=12345
+
+# verbindet sich mit dem Server und sendet eine Nachricht
+echo "Verbindung zum Server $SERVER_IP:$SERVER_PORT"
+echo "Nachricht:"
+read MSG
+echo "Sende Nachricht: $MSG"
+echo "$MSG" | ncat $SERVER_IP $SERVER_PORT
